92f6324fba1d4922338ed9057212b5bd3b7b046d
verockala <139936302+verockala@users.noreply.github.com>
2025-04-01 18:31:19 -0400

    Delete Proyecto Escritura en cache directory

--- Proyecto Escritura en cache/include/usos.h
+++
@@ -1,9 +0,0 @@
-#ifndef USOS_H
-#define USOS_H
-
-#include <iostream>
-
-extern const int TIEMPO_ACCESO_CACHE;
-extern const int TIEMPO_ACCESO_MEMORIA;
-
-#endif
--- Proyecto Escritura en cache/src/usos.cpp
+++
@@ -1,4 +0,0 @@
-#include "usos.h"
-
-const int TIEMPO_ACCESO_CACHE = 1;
-const int TIEMPO_ACCESO_MEMORIA = 10;
